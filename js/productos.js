c89a969baeeccb1b09ab533c6b2d91ac86b90864
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2023-04-18 23:30:12 -0300

    agregamos arreglos de carritos

--- js/productos.js
+++ js/productos.js
@@ -60,143 +60,131 @@ inputPrecioMaximo.addEventListener('input', function() {
     const precioMaximo = parseInt(this.value);
     filtrarPorPrecio(precioMaximo);
 });
-//talles
 
+//talles
 
 /* const talles = [40, 41, 42, 43, 44];
 const productosTalles = document.querySelectorAll('.card');
 const carrito = document.querySelector('.listado');
 const contadorCarrito = document.querySelector('#valorCarrito');
-const carritoCompras = JSON.parse(localStorage.getItem('carrito')) || [];
 
 let cantidadProductos = 0;
-let totalCarrito = 0;
-let divTotalCarrito = document.getElementById("totalCarrito");
+let carritoProductos = [];
 
-productosTalles.forEach(producto => {
+// Comprobar si hay datos en el Local Storage
+if (localStorage.getItem('carritoProductos')) {
+    carritoProductos = JSON.parse(localStorage.getItem('carritoProductos'));
+    cantidadProductos = carritoProductos.length;
+    actualizarCarrito();
+}
+
+function guardarProductosEnLocalStorage() {
+    localStorage.setItem('carritoProductos', JSON.stringify(carritoProductos));
+    localStorage.setItem('cantidadProductos', cantidadProductos);
+}
+
+function actualizarCarrito() {
+    carrito.innerHTML = '';
+    carritoProductos.forEach((producto, index) => {
+    const li = document.createElement('li');
+    li.classList.add('producto-carrito');
+    li.innerHTML = `
+        <span class="nombre">${producto.nombre}</span>
+        <span>Talle ${producto.talle}</span>
+        <span class="precio">${producto.precio}</span>
+        <button class="eliminar-producto" data-index="${index}">X</button>
+    `;
+    carrito.appendChild(li);
+    });
+    contadorCarrito.textContent = cantidadProductos;
+}
+
+productosTalles.forEach((producto,) => {
     const nombreProducto = producto.querySelector('.card-title').textContent;
     const precioProducto = producto.querySelector('.card-text').textContent;
     const botonesTalles = producto.querySelectorAll('.btn-outline-secondary');
 
-    let talleSeleccionado;
+let talleSeleccionado;
 
-    botonesTalles.forEach(botonTalle => {
-        botonTalle.addEventListener('click', () => {
-            botonesTalles.forEach(boton => {
-                boton.classList.remove('seleccionado');
-            });
-            botonTalle.classList.add('seleccionado');
-            talleSeleccionado = botonTalle.textContent;
-        });
-    });
-
-    const botonTarjeta = producto.querySelector('.btn-primary');
-    botonTarjeta.addEventListener('click', () => {
-        if (!talleSeleccionado) {
-            return;
-        }
-        const productoSeleccionado = `${nombreProducto} - ${precioProducto} - Talle ${talleSeleccionado}`;
-        carritoCompras.push(productoSeleccionado);
-        localStorage.setItem('carrito', JSON.stringify(carritoCompras));
-        const li = document.createElement('li');
-        li.classList.add('producto-carrito');
-        li.innerHTML = `
-        <span class="nombre">${nombreProducto}</span>
-        <span>Talle ${talleSeleccionado}</span>
-        <span class="precio">${precioProducto}</span>
-        <button class="eliminar-producto">X</button>
-        `;
-        carrito.appendChild(li);
-        talleSeleccionado = null;
+botonesTalles.forEach(botonTalle => {
+    botonTalle.addEventListener('click', () => {
         botonesTalles.forEach(boton => {
-            boton.classList.remove('seleccionado');
+        boton.classList.remove('seleccionado');
         });
-        cantidadProductos++;
-        contadorCarrito.textContent = cantidadProductos;
-
-        const precioProductoNumerico = parseFloat(precioProducto); 
-        totalCarrito += precioProductoNumerico;
-        divTotalCarrito.innerHTML = `Total: ${totalCarrito.toFixed(2)}`;
+        botonTalle.classList.add('seleccionado');
+        talleSeleccionado = botonTalle.textContent;
     });
 });
-carrito.addEventListener('click', (event) => {
-    if (event.target.classList.contains('eliminar-producto')) {
-        const productoAEliminar = event.target.parentNode;
-        const nombreProductoAEliminar = productoAEliminar.querySelector('.nombre').textContent;
-        const precioProductoAEliminar = productoAEliminar.querySelector('.precio').textContent;
-        const precioProductoAEliminarNumerico = parseFloat(precioProductoAEliminar);
-        const indiceProductoAEliminar = carritoCompras.findIndex(producto => producto.includes(nombreProductoAEliminar) && producto.includes(precioProductoAEliminar));
-        if (indiceProductoAEliminar !== -1) {
-            carritoCompras.splice(indiceProductoAEliminar, 1);
-            localStorage.setItem('carrito', JSON.stringify(carritoCompras));
-            productoAEliminar.remove();
-            cantidadProductos--;
-            contadorCarrito.textContent = cantidadProductos;
-            // Calcular el precio total del carrito a partir de los productos restantes
-            let totalCarritoActual = totalCarrito;
-            carritoCompras.forEach(producto => {
-                const precioProducto = producto.split(' - ')[1];
-                const precioNumerico = parseFloat(precioProducto);
-                if (!isNaN(precioNumerico)) {
-                    totalCarritoActual += precioNumerico;
-                }
-            });
-            totalCarrito = totalCarritoActual;
-            const divTotalCarrito = document.getElementById("totalCarrito");
-            divTotalCarrito.innerHTML = `Total: ${totalCarrito.toFixed(2)}`;
-        }
+
+const botonTarjeta = producto.querySelector('.btn-primary');
+botonTarjeta.addEventListener('click', () => {
+    if (!talleSeleccionado) {
+        return;
     }
-});
-if (carritoCompras.length > 0) {
-    carritoCompras.forEach(producto => {
-        const li = document.createElement('li');
-        li.classList.add('producto-carrito');
-        li.innerHTML = `
-            <span class="nombre">${producto.split(' - ')[0]}</span>
-            <span>${producto.split(' - ')[2]}</span>
-            <span class="precio">${producto.split(' - ')[1]}</span>
-            <button class="eliminar-producto">X</button>
-        `;
-        carrito.appendChild(li);
-        cantidadProductos++;
-        contadorCarrito.textContent = cantidadProductos;
-        const precioProductoNumerico = parseFloat(precioProducto.split(' - ')[1]); 
-        totalCarrito += precioProductoNumerico;
-    });
-    divTotalCarrito.innerHTML = `Total: ${totalCarrito.toFixed(2)}`; 
-} */
-const talles = [40, 41, 42, 43, 44];
-const productosTalles = document.querySelectorAll('.card');
-const carrito = document.querySelector('.listado');
-const contadorCarrito = document.querySelector('#valorCarrito');
-const carritoCompras = JSON.parse(localStorage.getItem('carrito')) || [];
-const divTotalCarrito = document.getElementById("totalCarrito");
-
-let cantidadProductos = 0;
-let totalCarrito = 0;
-
-
-function agregarProductoAlCarrito(nombreProducto, precioProducto, talleSeleccionado) {
-    const productoSeleccionado = `${nombreProducto} - ${precioProducto} - Talle ${talleSeleccionado}`;
-    carritoCompras.push(productoSeleccionado);
-    localStorage.setItem('carrito', JSON.stringify(carritoCompras));
+    const productoSeleccionado = {
+        nombre: nombreProducto,
+        precio: precioProducto,
+        talle: talleSeleccionado
+    };
+    carritoProductos.push(productoSeleccionado);
+    guardarProductosEnLocalStorage();
+    const index = carritoProductos.length - 1;
     const li = document.createElement('li');
     li.classList.add('producto-carrito');
     li.innerHTML = `
         <span class="nombre">${nombreProducto}</span>
         <span>Talle ${talleSeleccionado}</span>
         <span class="precio">${precioProducto}</span>
-        <button class="eliminar-producto">X</button>
+        <button class="eliminar-producto" data-index="${index}">X</button>
     `;
     carrito.appendChild(li);
+    talleSeleccionado = null;
+    botonesTalles.forEach(boton => {
+        boton.classList.remove('seleccionado');
+    });
     cantidadProductos++;
     contadorCarrito.textContent = cantidadProductos;
-    const precioProductoNumerico = parseFloat(precioProducto); 
-    totalCarrito += precioProductoNumerico;
-    divTotalCarrito.innerHTML = `Total: ${totalCarrito.toFixed(2)}`;
+    });
+});
+ */
+const talles = [40, 41, 42, 43, 44];
+const productosTalles = document.querySelectorAll('.card');
+const carrito = document.querySelector('.listado');
+const contadorCarrito = document.querySelector('#valorCarrito');
+const precioTotal = document.querySelector('#precioTotal');
+
+let cantidadProductos = 0;
+let carritoProductos = [];
+
+// Comprobar si hay datos en el Local Storage
+if (localStorage.getItem('carritoProductos')) {
+    carritoProductos = JSON.parse(localStorage.getItem('carritoProductos'));
+    cantidadProductos = carritoProductos.length;
+    actualizarCarrito();
 }
 
-productosTalles.forEach(producto => {
+function guardarProductosEnLocalStorage() {
+    localStorage.setItem('carritoProductos', JSON.stringify(carritoProductos));
+    localStorage.setItem('cantidadProductos', cantidadProductos);
+}
+
+function actualizarCarrito() {
+    carrito.innerHTML = '';
+    carritoProductos.forEach((producto, index) => {
+        const li = document.createElement('li');
+        li.classList.add('producto-carrito');
+        li.innerHTML = `
+            <span class="nombre">${producto.nombre}</span>
+            <span>Talle ${producto.talle}</span>
+            <span class="precio">${producto.precio}</span>
+            <button class="eliminar-producto" data-index="${index}">X</button>
+        `;
+        carrito.appendChild(li);
+    });
+    contadorCarrito.textContent = cantidadProductos;
+}
+
+productosTalles.forEach((producto,) => {
     const nombreProducto = producto.querySelector('.card-title').textContent;
     const precioProducto = producto.querySelector('.card-text').textContent;
     const botonesTalles = producto.querySelectorAll('.btn-outline-secondary');
@@ -218,45 +206,49 @@ productosTalles.forEach(producto => {
         if (!talleSeleccionado) {
             return;
         }
-        agregarProductoAlCarrito(nombreProducto, precioProducto, talleSeleccionado);
+        const productoSeleccionado = {
+            nombre: nombreProducto,
+            precio: precioProducto,
+            talle: talleSeleccionado
+        };
+        carritoProductos.push(productoSeleccionado);
+        guardarProductosEnLocalStorage();
+        const index = carritoProductos.length - 1;
+        const li = document.createElement('li');
+        li.classList.add('producto-carrito');
+        li.innerHTML = `
+            <span class="nombre">${nombreProducto}</span>
+            <span>Talle ${talleSeleccionado}</span>
+            <span class="precio">${precioProducto}</span>
+            <button class="eliminar-producto" data-index="${index}">X</button>
+        `;
+        carrito.appendChild(li);
         talleSeleccionado = null;
         botonesTalles.forEach(boton => {
             boton.classList.remove('seleccionado');
         });
+        cantidadProductos++;
+        contadorCarrito.textContent = cantidadProductos;
+        actualizarPrecioTotal();
     });
 });
 
+function actualizarPrecioTotal() {
+    let precioTotalCarrito = 0;
+    const preciosProductos = document.querySelectorAll('.precio');
+    preciosProductos.forEach(precioProducto => {
+        const precio = parseFloat(precioProducto.textContent.replace('$', ''));
+        precioTotalCarrito += precio;
+    });
+    precioTotal.textContent = `$${precioTotalCarrito.toFixed(2)}`;
+}
+
 carrito.addEventListener('click', (event) => {
     if (event.target.classList.contains('eliminar-producto')) {
-        const productoAEliminar = event.target.parentNode;
-        const nombreProductoAEliminar = productoAEliminar.querySelector('.nombre').textContent;
-        const precioProductoAEliminar = productoAEliminar.querySelector('.precio').textContent;
-        const precioProductoAEliminarNumerico = parseFloat(precioProductoAEliminar);
-        const indiceProductoAEliminar = carritoCompras.findIndex(producto => producto.includes(nombreProductoAEliminar) && producto.includes(precioProductoAEliminar));
-        if (indiceProductoAEliminar !== -1) {
-            carritoCompras.splice(indiceProductoAEliminar, 1);
-            localStorage.setItem('carrito', JSON.stringify(carritoCompras));
-            productoAEliminar.remove();
-            cantidadProductos--;
-            contadorCarrito.textContent = cantidadProductos;
-            // Calcular el precio total del carrito a partir de los productos restantes
-            let totalCarritoActual = totalCarrito;
-            carritoCompras.forEach(producto => {
-                const precioProducto = producto.split(' - ')[1];
-                const precioNumerico = parseFloat(precioProducto);
-                if (!isNaN(precioNumerico)) {
-                    totalCarritoActual += precioNumerico;
-                }
-            });
-            totalCarrito = totalCarritoActual;
-            const divTotalCarrito = document.querySelector("#totalCarrito");
-            divTotalCarrito.innerHTML = `Total: ${totalCarrito.toFixed(2)}`;
-        }
+    const index = event.target.dataset.index;
+    carritoProductos.splice(index, 1);
+    guardarProductosEnLocalStorage();
+    cantidadProductos--;
+    actualizarCarrito();
     }
-});
-
-
-
-
-
-
+}); 
